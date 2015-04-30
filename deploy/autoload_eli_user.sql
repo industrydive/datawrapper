71a73c3092d3ed2bd3ad30f2634c444a53e6b28a
Eli Dickinson <eli@industrydive.com>
2015-04-30 10:04:03 -0400

    add eli user as admin

--- deploy/autoload_eli_user.sql
+++ deploy/autoload_eli_user.sql
@@ -1,1 +1,2 @@
 INSERT INTO `user` VALUES (1,'eli@industrydive.com','68013019fdf64fc4dd29109c3f01139eafb39b987b08c0662d93f8c1a2c765de','4ab87550149f05a028fdc96e2b45adc730878024ce26503e9c05077428a549f1',NULL,1,0,'en_US','2015-04-29 17:11:45',NULL,NULL,NULL,NULL);
+update `user` set role=0 where email = 'eli@industrydive.com';
