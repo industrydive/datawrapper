75a726ad9b0ac9d7687a625e684b6fa0fff87bdd
Eli Dickinson <eli@industrydive.com>
2015-05-01 16:46:30 -0400

    selectively load plugins

--- deploy/load_sql_and_plugins.sh
+++ deploy/load_sql_and_plugins.sh
@@ -15,8 +15,22 @@ cat /deploy/setup_db.sql /srv/datawrapper/lib/core/build/sql/schema.sql /deploy/
 
 echo "=> Inital SQL Loaded"
 
-# loading plugins requires database to be up
-php scripts/plugin.php install "*"
+# loading plugins requires database to be up so we do it hre
+php scripts/plugin.php install "admin*"
+php scripts/plugin.php install "core*"
+php scripts/plugin.php install "phantomjs"
+php scripts/plugin.php install "email-native" # req'd for export-image
+php scripts/plugin.php install "export*"
+php scripts/plugin.php install "gallery"
+php scripts/plugin.php install "publish-s3"
+php scripts/plugin.php install "theme-default"
+php scripts/plugin.php install "theme-dive*"
+php scripts/plugin.php install "visualization"
+php scripts/plugin.php install "visualization-raphael-chart" # req'd for bar chart
+php scripts/plugin.php install "visualization-bar-chart"
+php scripts/plugin.php install "visualization-column-chart"
+php scripts/plugin.php install "visualization-line-chart"
+
 
 echo "=> Datawrapper Plugins Loaded"
 
